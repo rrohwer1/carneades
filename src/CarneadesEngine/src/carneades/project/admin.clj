d5e8ad0c5576d1e5195bad9a7eff5505ab8380fa
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-04-05 14:43:45 +0200

    function to list projects

--- src/CarneadesEngine/src/carneades/project/admin.clj
+++ src/CarneadesEngine/src/carneades/project/admin.clj
@@ -2,5 +2,21 @@
 ;;; Licensed under the EUPL V.1.1
 
 (ns ^{:doc "Management functions for the projects."}
-  carneades.project.admin)
+  carneades.project.admin
+  (:use [carneades.engine.utils :only [file-separator exists?]])
+  (:require [carneades.config.config :as config]))
 
+(defn- project?
+  "Returns true if the director is a project."
+  [dir]
+  (and (.isDirectory dir)
+       (exists? (str dir file-separator "properties.clj"))))
+
+(defn list-projects
+  "Returns a list of project names. A read on the disk is performed."
+  []
+  (let [projects-directory (config/properties :projects-directory)
+        dirs (into [] (.listFiles (clojure.java.io/file
+                                   projects-directory)))
+        projects (filter project? dirs)]
+    (map (memfn getName) projects)))
