153f33661d78eef00ca494600265213b6973d3bd
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-04-17 12:12:32 +0200

    test creating a new main statement passing

--- src/CarneadesWebApp/casper/run-scenario-test.js
+++ src/CarneadesWebApp/casper/run-scenario-test.js
@@ -13,9 +13,7 @@ var test_scenario_entry = function() {
     }, 'Introduction page contains some text');
 
 
-    casper.then(function() {
-        this.click('#start');
-    });
+    this.click('#start');
 
     casper.waitForSelector('#issues', test_scenario_issues);
 };
@@ -27,9 +25,7 @@ var test_scenario_issues = function() {
         return $('body p:first').text().match('the following issues') != null;
     }, 'Issues page contains some text');
 
-    casper.then(function() {
-        this.click('#submit');
-    });
+    this.click('#submit');
 
     casper.waitForSelector('#facts', test_scenario_facts);
 
@@ -42,9 +38,8 @@ var test_scenario_facts = function() {
         return $('body p:first').text().match('be used for commercial') != null;
     }, 'The fact page contains a question about the use of the work');
 
-    casper.then(function() {
-        this.click('input[type=button]');
-    });
+
+    this.click('input[type=button]');
 
     casper.waitForSelector('h3:nth-of-type(1)', test_scenario_facts_license);
     
@@ -56,13 +51,12 @@ var test_scenario_facts_license = function() {
     }, 'The fact page contains a question about the license');
 
     // selects 'no' for the license question
-    casper.thenEvaluate(function(term) {
+    casper.evaluate(function(term) {
         document.querySelector('input[name="input-q2-2"][value="no"]').setAttribute('checked', true);
     });
 
-    casper.then(function() {
-        this.click('input[type=button]');
-    });
+    this.click('input[type=button]');
+
 
     casper.waitForSelector('h3:nth-of-type(2)', test_scenario_facts_search);
 };
@@ -77,13 +71,11 @@ var test_scenario_facts_search = function() {
     }, 'The fact page contains a question about an announcement');
 
     // selects 'yes' for the announcement question
-    casper.thenEvaluate(function(term) {
+    casper.evaluate(function(term) {
         document.querySelector('input[name="input-q4-5"][value="yes"]').setAttribute('checked', true);
     });
 
-    casper.then(function() {
-        this.click('input[type=button]');
-    });
+    this.click('input[type=button]');
 
     casper.waitForSelector('#argumentgraph', test_scenario_outline);
 };
@@ -93,9 +85,7 @@ var test_scenario_outline = function() {
         return $('p:first').text() == "☐ The person may publish the work.";
     }, 'The outline page shows that the main issue is not acceptable');
 
-    casper.then(function() {
-        this.click('#schemes-item');
-    });
+    this.click('#schemes-item');
 
     casper.waitForSelector('.theory-view', test_scenario_schemes);
     // casper.then(function() {
@@ -109,18 +99,14 @@ var test_scenario_schemes = function() {
         return $('#outline li:first a').text().match('Argument from') != null;
     }, 'There is a least one scheme on the schemes page named Argument from..');
 
-    casper.then(function() {
-        this.click('#policies-item');
-    });
+    this.click('#policies-item');
 
     casper.waitForSelector('.policies-filtering', test_scenario_policies);
 };
 
 var test_scenario_policies = function() {
-    casper.then(function() {
-        this.click('#in');
-        this.waitWhileSelector('#UrhG', test_scenario_policies_aktionbundnis);
-    });
+    this.click('#in');
+    this.waitWhileSelector('#UrhG', test_scenario_policies_aktionbundnis);
 
 };
 
@@ -129,9 +115,7 @@ var test_scenario_policies_aktionbundnis = function() {
         return $('input[type=submit]').length == 1;
     }, 'Only one policy makes the issue acceptable');
 
-    casper.then(function() {
-        this.click('#inputQ12-Aktionsbundnis');
-    });
+    this.click('#inputQ12-Aktionsbundnis');
 
     casper.waitForSelector('#argumentgraph', 
                            test_scenario_outline_after_eval,
@@ -146,18 +130,14 @@ var test_scenario_outline_after_eval = function() {
         return $('p:first').text() == "☑ The person may publish the work.";
     }, 'The outline page shows that the main issue is now acceptable');
 
-    casper.then(function() {
-        this.click('#facts-item');
-    });
+    this.click('#facts-item');
 
     casper.waitForText('Modify the facts.', test_scenario_change_facts);
 
 };
 
 var test_scenario_change_facts = function() {
-    casper.then(function() {
-        this.click('a[href="#/facts/modify"]');
-    });
+    this.click('a[href="#/facts/modify"]');
 
     casper.waitForText('You can modify the answers below', test_scenario_change_facts_license);
 };
@@ -165,13 +145,11 @@ var test_scenario_change_facts = function() {
 var test_scenario_change_facts_license = function() {
 
     // selects 'yes' for the license question
-    casper.thenEvaluate(function(term) {
+    casper.evaluate(function(term) {
         document.querySelector('input[name="input-q3-7"][value=yes]').setAttribute('checked', true);
     });
 
-    casper.then(function() {
-        this.click('input[type=button][value=Submit]');
-    });
+    this.click('input[type=button][value=Submit]');
 
     casper.waitForSelector('#argumentgraph', 
                            test_scenario_select_urhg,
@@ -182,18 +160,14 @@ var test_scenario_change_facts_license = function() {
 };
 
 var test_scenario_select_urhg = function() {
-    casper.then(function() {
-        this.click('#policies-item');
-    });
+    this.click('#policies-item');
 
     casper.waitForSelector('.policies-filtering', test_scenario_select_urhg2);
 };
 
 
 var test_scenario_select_urhg2 = function() {
-    casper.then(function() {
-        this.click('#inputUrhG');
-    });
+    this.click('#inputUrhG');
 
     casper.waitForSelector('#argumentgraph', 
                            test_scenario_copy,
@@ -204,9 +178,7 @@ var test_scenario_select_urhg2 = function() {
 };
 
 var test_scenario_copy = function () {
-    casper.then(function() {
-        this.click('#copy');
-    });
+    this.click('#copy');
 
     casper.setFilter("page.confirm", function(msg) {
         return msg === "Make a copy of the current case?" ? true : false;
@@ -220,26 +192,20 @@ var test_scenario_copy = function () {
 };
 
 var test_scenario_vote = function () {
-    casper.then(function () {
-        this.click('.vote');
-    });
+    this.click('.vote');
 
    casper.waitForText('Given the facts of this case', test_scenario_vote2);
 };
 
 var test_scenario_vote2 = function () {
-    casper.then(function () {
-        this.click('.vote-now');
-    });
+    this.click('.vote-now');
 
     casper.waitForText('Thank you for your vote!', test_scenario_vote3);
 
 };
 
 var test_scenario_vote3 = function () {
-    casper.then(function () {
-        this.click('.show-vote-results');
-    });
+    this.click('.show-vote-results');
 
     casper.waitForText('Claim', test_scenario_vote4);
 };
@@ -249,17 +215,13 @@ var test_scenario_vote4 = function () {
        return $('tr td:nth-of-type(2)').text() == "100.00%";
     }, 'The vote has been registered');
 
-    casper.then(function() {
-        this.click('#arguments-item');
-    });
+    this.click('#arguments-item');
 
     casper.waitForSelector('#argumentgraph', test_scenario_newstatement)
 };
 
 var test_scenario_evaluation = function () {
-    casper.then(function () {
-        this.click('.evaluate');
-    });
+    this.click('.evaluate');
 
     casper.waitForText('Evaluation finished', function() {
         casper.waitForSelector('#newstatement', test_scenario_newstatement);
@@ -267,61 +229,51 @@ var test_scenario_evaluation = function () {
 };
 
 var test_scenario_newstatement = function () {
-    casper.then(function () {
-        this.click('#newstatement');
-    });
+    this.click('#newstatement');
 
     casper.waitForSelector('#save-statement', test_scenario_newstatement2);
 };    
 
 var test_scenario_newstatement2 = function () {
     this.test.assertExist('input[name=main][value=yes]');
+    
+    this.click('input[name=main][value=yes]');
+    
+    casper.evaluate(function(term) {
+        var text_en = "Here some text!";
+        var metadata_en = "Here some metadata";
 
-    casper.thenEvaluate(function(term) {
-        document.querySelector('input[name=main][value=yes]').setAttribute('checked', true);
-    });
-
-    // this.test.assertEval(function() {
-    //     return document.querySelector('input[name=main][value=yes]').getAttribute('checked') == "true";
-    // }, 'Main was set to true');
-
-    casper.then(function () {
-        this.click('#save-statement'); 
+        document.querySelector('#statement-editor-text').value = text_en;  
+        document.querySelector('.metadata-description-input').value = metadata_en;
     });
     
-    casper.waitWhileSelector('#save-statement', test_scenario_newstatement4)
+    
+
+    this.test.assertEval(function() {
+        return document.querySelector('input[name=main]:checked').getAttribute('value') == "yes";
+    }, 'Main was set to true');
+
+    // take_picture.call(this, '/tmp/carneades_before_save.png');
+    
+    this.click('#save-statement'); 
+    
+    casper.waitForText('Here some text!', test_scenario_newstatement3);
+    
 };
 
-
-// var text_en = "Here some text!";
-// var metadata_en = "Here some metadata";
-
-// document.querySelector('#statement-editor-text').value = text_en;  
-// document.querySelector('.metadata-description-input').value = metadata_en;
-
-// var test_scenario_newstatement3 = function () {
-
+var test_scenario_newstatement3 = function () {
+    // take_picture.call(this, '/tmp/carneades_after_save.png');
     
-//     casper.waitForText('Here some text!', test_scenario_newstatement4);
-    
-// };
+    this.click('#newargument');
 
-var test_scenario_newstatement4 = function () {
-    take_picture.call(this);
-
-    casper.then(function () {
-        this.click('#newargument');
-    });
-
-    casper.waitForSelector('.save-argument', test_scenario_newargument);
+    casper.waitForSelector('.save-argument', test_scenario_newargument);    
 };
 
 var test_scenario_newargument = function () {
-    casper.thenEvaluate(function (term) {
+    casper.evaluate(function (term) {
         $('#argument-editor-scheme').select2("val");
     });
 
-    // take_picture.call(this);
 };
 
 // TODO test creating an argument
@@ -329,9 +281,9 @@ var test_scenario_newargument = function () {
 // TODO export
 // TODO test report
 
-var take_picture = function () {
+var take_picture = function (filename) {
 
-    this.capture('/tmp/carneades.png', {
+    this.capture(filename, {
         top: 0,
         left: 0,
         width: 1024,
