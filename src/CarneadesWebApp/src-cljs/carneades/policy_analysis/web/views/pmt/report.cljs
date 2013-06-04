3105748d6e200b6a1e7907de4ffa2f856ca8b8c1
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-06-04 16:52:30 +0200

    fix menu for the report page

--- src/CarneadesWebApp/src-cljs/carneades/policy_analysis/web/views/pmt/report.cljs
+++ src/CarneadesWebApp/src-cljs/carneades/policy_analysis/web/views/pmt/report.cljs
@@ -6,7 +6,8 @@
         [jayq.core :only [$ inner attr]]
         [carneades.analysis.web.views.core :only [template]]
         [carneades.policy-analysis.web.views.pmt.vote :only [round-score]])
-  (:require [carneades.analysis.web.backbone.core :as bb])
+  (:require [carneades.analysis.web.backbone.core :as bb]
+            [carneades.analysis.web.views.header :as header])
   (:require-macros [carneades.analysis.web.backbone.macros :as bbm]
                    [carneades.policy-analysis.web.views.menu :as menu]))
 
@@ -36,6 +37,21 @@
 (defn ^:export display
   [project]
   (js/PM.load_project project)
+  (header/show {:text (.get js/PM.project "title")
+                :link (str "#/project/" js/PM.project.id)}
+               [{:text :pmt_menu_intro
+                 :link (str "#/policies/introduction/" js/PM.project.id)}
+                {:text :pmt_menu_issues
+                 :link (str "#/policies/issues/" js/PM.project.id)}
+                {:text :pmt_menu_facts
+                 :link (str "#/policies/facts/" js/PM.project.id)}
+                {:text :pmt_menu_policies
+                 :link (str "#/policies/policies/" js/PM.project.id)}
+                {:text :pmt_menu_analysis
+                 :link (str "#/arguments/outline/" js/PM.project.id "/" js/IMPACT.db)}
+                {:text :pmt_menu_report
+                 :link (str "#/policies/report/" js/PM.project.id "/" js/IMPACT.db)}
+                ])
   (let [report-view (bb/new
                      Report
                      {:model (bb/new-model
