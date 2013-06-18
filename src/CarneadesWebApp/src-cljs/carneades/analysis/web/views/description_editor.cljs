1cc4ed649cfab02a419ddef27affccd2eb186557
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-06-18 15:45:00 +0200

    CLJS description editor working with multiple language

--- src/CarneadesWebApp/src-cljs/carneades/analysis/web/views/description_editor.cljs
+++ src/CarneadesWebApp/src-cljs/carneades/analysis/web/views/description_editor.cljs
@@ -4,20 +4,65 @@
 (ns carneades.analysis.web.views.description-editor
   (:use [jayq.core :only [$ inner attr append]]
         [jayq.util :only [log]])
-  (:refer-clojure :exclude [get]))
+  (:refer-clojure :exclude [get])
+  (:require [carneades.analysis.web.dispatch :as dispatch]))
+
+(def state (atom {:lang :en
+                  :value {}}))
+
+(defn update-state
+  ;; assigns selected lang to the new selected language
+  ;; and the actual description to the old one
+  [state new-lang description]
+  (let [previous-lang (:lang state)]
+   (-> state
+       (assoc :lang new-lang)
+       (assoc-in [:value previous-lang] description))))
+
+(defn get-lang
+  [a]
+  (let [href (.attr ($ a) "href")
+        lang (subs href (- (count href) 2))]
+    (keyword lang)))
+
+(defn select-tab
+  [lang]
+  (doseq [li ($ ".description-editor > ul > li")]
+    (let [li-el ($ li)]
+     (if (= (get-lang (.find li-el "a")) lang)
+       (do
+         (.addClass li-el "ui-tabs-selected")
+         (.addClass li-el "ui-state-active"))
+       (do
+         (.removeClass li-el "ui-tabs-selected")
+         (.removeClass li-el "ui-state-active"))))))
+
+(defn on-description-tab-changed
+  [_ msg]
+  (swap! state update-state (:lang msg) (:description msg))
+  (let [s (deref state)]
+    (.val ($ ".description") (get-in s [:value (:lang msg)] "")))
+  (select-tab (:lang msg)))
+
+(dispatch/react-to #{::description-tab-changed}
+                   on-description-tab-changed)
 
 (defn get
-  [])
+  []
+  (let [{:keys [lang value]} (deref state)
+        description (.val ($ ".description"))]
+   (merge value
+          {lang description})))
 
 (defn- on-tab-selected
   [event ui]
   (log "tab-selected: ")
   (log ui)
   (let [tab (.-tab ui)
-        href (.attr ($ tab) "href")
-        lang (subs href (- (count href) 2))]
-    (log "lang=" )
-    (log lang))
+        lang (get-lang tab)]
+    (dispatch/fire ::description-tab-changed
+                   {:lang lang
+                    :description (.val ($ ".description"))}))
   false)
 
 (defn show
